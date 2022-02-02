9f92c472bdade038e40022cdd74a71e2b4a2ec54
Umesh162 <Umeshpalival@gmail.com>
2022-02-02 13:26:48 +0530

    first commit

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,92 @@
+import Note from "../models/noteModel.js";
+import asyncHandler from "express-async-handler";
+
+//     Get logged in user notes
+//    GET /api/notes
+//   Private
+const getNotes = asyncHandler(async (req, res) => {
+  const notes = await Note.find({ user: req.user._id });
+  res.json(notes);
+});
+
+//  Fetch single Note
+// GET /api/notes/:id
+//  Public
+const getNoteById = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
+
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).json({ message: "Note not found" });
+  }
+
+  res.json(note);
+});
+
+//     Create single Note
+//           GET /api/notes/create
+//          Private
+const CreateNote = asyncHandler(async (req, res) => {
+  const { title, content, category } = req.body;
+
+  if (!title || !content || !category) {
+    res.status(400);
+    throw new Error("Please Fill all the feilds");
+    return;
+  } else {
+    const note = new Note({ user: req.user._id, title, content, category });
+
+    const createdNote = await note.save();
+
+    res.status(201).json(createdNote);
+  }
+});
+
+//     Delete single Note
+//           GET /api/notes/:id
+//          Private
+const DeleteNote = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
+
+  if (note.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You can't perform this action");
+  }
+
+  if (note) {
+    await note.remove();
+    res.json({ message: "Note Removed" });
+  } else {
+    res.status(404);
+    throw new Error("Note not Found");
+  }
+});
+
+//     Update a note
+//    PUT /api/notes/:id
+//   Private
+const UpdateNote = asyncHandler(async (req, res) => {
+  const { title, content, category } = req.body;
+
+  const note = await Note.findById(req.params.id);
+
+  if (note.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    throw new Error("You can't perform this action");
+  }
+
+  if (note) {
+    note.title = title;
+    note.content = content;
+    note.category = category;
+
+    const updatedNote = await note.save();
+    res.json(updatedNote);
+  } else {
+    res.status(404);
+    throw new Error("Note not found");
+  }
+});
+
+export { getNoteById, getNotes, CreateNote, DeleteNote, UpdateNote };
